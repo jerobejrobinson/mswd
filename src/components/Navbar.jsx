59a3408e6a40d5db77ef84d11e877ed376e4565b
jerobejrobinson <jerobejrobinson@gmail.com>
2023-11-01 16:54:35 -0500

    working on menu

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Navbar() {
+    return (
+        <nav id="mobile-menu">
+            <img src="https://ucarecdn.com/dbe39d68-f5f5-4692-9957-10299d4f650e/-/preview/500x500/-/quality/smart_retina/-/format/auto/" alt="Mid-South Web Designs Logo" />
+            <div  class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>  
+    )
+}
